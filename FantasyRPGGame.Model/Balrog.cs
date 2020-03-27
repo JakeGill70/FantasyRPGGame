3d828a13580346017b48149ae59254ac8c57810f
Jake Gillenwater <JakeGill70@gmail.com>
2020-03-27 03:12:10 -0400

    Milestone 5 - Balrogs

---
+++ FantasyRPGGame.Model/Balrog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FantasyRPGGame.Model
+{
+    public class Balrog : Demon
+    {
+        public override string Race { get { return "Balrog"; } }
+
+        public Balrog(IRandom random) : base(random)
+        {
+        }
+
+        public override int CalculateDamage()
+        {
+            int dmg = 0;
+
+            // Belogs are deadly and inflict base damage twice
+            // Formula taken from the Creature Class
+            dmg += _random.Next(Strength) + 1; // Base Damage Once
+            dmg += _random.Next(Strength) + 1; // Base Damage Twice
+
+            // 25% for extra 10 damage
+            // Spec taken from demon
+            if (_random.Next(100) < 25)
+            {
+                dmg += 10;
+            }
+
+            return dmg;
+        }
+    }
+}
---
+++ FantasyRPGGame.UnitTesting/ABalrog.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using FantasyRPGGame.Model;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FantasyRPGGame.UnitTesting
+{
+    [TestFixture]
+    public class ABalrog
+    {
+        [Test]
+        [Category("Milestone 5")]
+        public void ShouldHaveARaceOfBalrog()
+        {
+            // Arrange
+            var mock_random = new Mock<IRandom>();
+
+            var sut = new Balrog(mock_random.Object);
+
+            // Action
+            var actual = sut.Race;
+
+            // Assert
+            var expected = "Balrog";
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Category("Milestone 5")]
+        public void ShouldCalculateBaseDamageTwice()
+        {
+            // Arrange
+            var mock_random = new Mock<IRandom>();
+            mock_random.SetupSequence(o => o.Next(It.IsAny<int>()))
+                .Returns(0)
+                .Returns(0)
+                .Returns(100);
+            
+            var sut = new Balrog(mock_random.Object);
+            sut.Strength = 0;
+
+            // Action
+            var actual = sut.CalculateDamage();
+
+            // Assert
+            var expected = 2;
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
